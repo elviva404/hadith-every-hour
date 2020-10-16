f58b4a4ea167344cf0a8c2a1c8eee43b8242ff1b
Ananto30 <azizulhaq.ananto@gmail.com>
2020-10-16 17:37:14 +0600

    Add hadith tracking

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import requests
 from dataclasses import dataclass
 import tweepy
 import os
+import json
 
-hadith_book = "bukhari"
-hadith_number = os.getenv("HADITH_NUMBER")
 
-hadith_source = "http://askhadith.herokuapp.com"
-full_hadith_source = "https://askhadith.herokuapp.com"
-hadith_link = f"{full_hadith_source}/{hadith_book}/{hadith_number}"
 tweet_char_limit = 260
 
 
@@ -21,12 +17,13 @@ class Hadith:
     narrator: str
     content: str
     hadith_number: str
+    hadith_link: str = None
 
 
 def make_tweet(hadith: Hadith):
     full_hadith = "\n".join([hadith.narrator, hadith.content, hadith.hadith_number])
     if len(full_hadith) > tweet_char_limit:
-        link = f"\nFull hadith: {hadith_link}"
+        link = f"\nFull hadith: {hadith.hadith_link}"
         full_hadith = full_hadith[: (tweet_char_limit - (len(link) + 3))] + "..." + link
 
     return full_hadith
@@ -45,11 +42,11 @@ def chunk_tweet(hadith: Hadith):
     chunks = []
     while i < len(full_hadith):
         j += tweet_char_limit
-        if j < len(full_hadith) and full_hadith[j + 1] != " ":
+        if j < len(full_hadith) and full_hadith[j] != " ":
             j = get_prev_word_end_index(j, full_hadith)
         chunks.append(full_hadith[i:j])
         i = j
-    link = f"\nFor convenient reading: {hadith_link}"
+    link = f"\nFor convenient reading: {hadith.hadith_link}"
     if len(chunks[-1]) < (tweet_char_limit - len(link)):
         chunks[-1] = chunks[-1] + link
     else:
@@ -81,14 +78,30 @@ def tweet(hadith: Hadith):
             f"@HadithEveryHour {chunks[i]}", in_reply_to_status_id=status.id
         )
 
+
 def tweet_hadith():
+
+    with open("hadith_track.json", "r") as json_file:
+        data = json.load(json_file)
+        hadith_book = data.get("hadith_book")
+        hadith_number = int(data.get("hadith_number"))
+
+    hadith_source = "http://askhadith.herokuapp.com"
+    full_hadith_source = "https://askhadith.herokuapp.com"
+    hadith_link = f"{full_hadith_source}/{hadith_book}/{hadith_number}"
+
     resp = requests.get(f"{hadith_source}/api/{hadith_book}/{hadith_number}")
     if resp.json():
         hadith = Hadith(**resp.json())
+        hadith.hadith_link = hadith_link
+        print_chunks(hadith)
+        # tweet(hadith)
         hadith_number += 1
-
-
+        with open("hadith_track.json", "w") as json_file:
+            json_file.write(
+                json.dumps({"hadith_book": hadith_book, "hadith_number": hadith_number})
+            )
 
 
 if __name__ == "__main__":
-    tweet(hadith)
+    tweet_hadith()
